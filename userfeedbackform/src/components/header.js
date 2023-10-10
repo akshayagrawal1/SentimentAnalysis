bbbeac6c67bf39ec00524143782fa0c576c3b0b7
tribhu2001 <79187488+tribhu2001@users.noreply.github.com>
2023-10-10 12:05:47 +0530

    Header Change

--- userfeedbackform/src/components/header.js
+++ userfeedbackform/src/components/header.js
@@ -4,10 +4,11 @@ import Navbar from 'react-bootstrap/Navbar';
 function StickyHeader() {
   return (
     <>
-      <Navbar bg="light" sticky="top">
-        <Container>
-          <Navbar.Brand href="#home">Feedback Form</Navbar.Brand>
-        </Container>
+      <Navbar style={{background: "linear-gradient(270deg,gold,blue,green)", animation: "2s infinite alternate linear"}} sticky="top">
+          <div class="container" style={{marginLeft: "28%"}}>
+            <a ><img
+              src="https://cdn.pixabay.com/photo/2017/09/06/17/57/feedback-2722425_1280.jpg" width="600" height="50" /></a>
+          </div>
       </Navbar>
       <br />
     </>
